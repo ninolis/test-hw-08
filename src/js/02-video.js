4d028f278b973d8e0a4028941dc149caf6de59c7
ninolis <97913013+ninolis@users.noreply.github.com>
2022-12-15 13:19:24 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,22 @@
+'use strict';
 
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const playerCurrentTime = `videoplayer-current-time`;
+
+let timeStamp = 0;
+const storedTime = localStorage.getItem(playerCurrentTime);
+if (storedTime) {
+  player.setCurrentTime(storedTime);
+}
+player.on(
+  'timeupdate',
+  throttle(data => {
+    timeStamp = data[`seconds`];
+    localStorage.setItem(playerCurrentTime, timeStamp);
+    console.log(timeStamp);
+  }, 1000)
+);
