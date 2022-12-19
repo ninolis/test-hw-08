0607d1dff9ce28416203aa403e5c8be10ae6481e
ninolis <97913013+ninolis@users.noreply.github.com>
2022-12-19 10:08:12 +0200

    video & feedback update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,17 +5,18 @@ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
-const playerCurrentTime = `videoplayer-current-time`;
+const playerCurrentTime = 'videoplayer-current-time';
 
 let timeStamp = 0;
 const storedTime = localStorage.getItem(playerCurrentTime);
 if (storedTime) {
   player.setCurrentTime(storedTime);
 }
+
 player.on(
   'timeupdate',
   throttle(data => {
-    timeStamp = data[`seconds`];
+    timeStamp = data['seconds'];
     localStorage.setItem(playerCurrentTime, timeStamp);
     console.log(timeStamp);
   }, 1000)
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+'use strict';
 
+import throttle from 'lodash.throttle';
+const LOCALSTORAGE_KEY = `feedback-form-state`;
+const feedbackForm = document.querySelector('.feedback-form');
+const feedbackFormInput = document.querySelector('.feedback-form input');
+const feedbackFormTextarea = document.querySelector('.feedback-form textarea');
+
+const feedbackFormData = {};
+
+feedbackForm.addEventListener(`submit`, onFormSubmit);
+feedbackForm.addEventListener(`input`, throttle(onUserInput, 500));
+
+if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+  feedbackFormData = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+  feedbackFormInput.value = feedbackFormData.email ?? '';
+  feedbackFormTextarea.value = feedbackFormData.message ?? '';
+}
